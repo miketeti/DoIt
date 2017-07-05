9e1a47cd5c29fa6e9940654c13953d516497b731
Michael Teti <mteti@optimalcapacity.com>
2017-07-05 11:43:44 -0400

    CoreData - DoIt ToDo List - Lesson 6 Setting things up

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Michael Teti on 7/5/17.
+//  Copyright © 2017 Michael Teti. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
