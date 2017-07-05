303ecefcaae64ee0f42e3f02572f593f07f60076
Michael Teti <mteti@optimalcapacity.com>
2017-07-05 11:30:53 -0400

    CoreData - DoIt ToDo List - Lesson 5 Segue

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -57,5 +57,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return [task1,task2,task3]
     }
     
+    @IBAction func plusTapped(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
+    
+    
+    
 }
 
