04f0cdb373eb383f3c2a6e3a55ca28e94c08c750
Michael Teti <mteti@optimalcapacity.com>
2017-07-05 11:16:59 -0400

    CoreData - DoIt ToDo List - Lesson 4 Task Class

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Michael Teti on 7/5/17.
+//  Copyright © 2017 Michael Teti. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
